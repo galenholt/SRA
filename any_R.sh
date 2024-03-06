b1b3e447f8f211524cdb354478a31021505f86ee
Galen Holt <53928785+galenholt@users.noreply.github.com>
2024-03-06 15:58:43 +1100

    I don't think gandalf actually cares much about time, so just make them big.

--- any_R.sh
+++ any_R.sh
@@ -2,7 +2,7 @@
 
 # # Resources on test system: 20 nodes, each with 12 cores. 70GB RAM
 
-#SBATCH --time=0:15:00 # request time (walltime, not compute time)
+#SBATCH --time=4:00:00 # request time (walltime, not compute time)
 #SBATCH --mem=500MB # request memory. This is just a coordinator, so shouldn't need its own memory
 #SBATCH --nodes=1 # number of nodes. Need > 1 to test utilisation
 #SBATCH --ntasks-per-node=1 # Cores per node
