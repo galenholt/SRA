99fc5c878a6cdc8b486a4ce8d940ec89fa79174e
Galen Holt <53928785+galenholt@users.noreply.github.com>
2024-05-01 21:13:15 +1000

    get the ms outline running all the way through with new data

---
+++ R/catch_cat_sf.R
@@ -0,0 +1,25 @@
+#' Helper to read in and concatenate sfs for each catchment from the standard output structures
+#'
+#' @param which_strict the name of the stricture level (e.g. 'germ_area')
+#' @param outerdir the directory (e.g. 'black_box')
+#' @param catchments The set of catchments
+#'
+#' @return
+#' @export
+#'
+#' @examples
+catch_cat_sf <- function(which_strict, outerdir, catchments = 'all') {
+  if ('all' %in% catchments) {
+    cleancatch <- CC2::ltimNoNorth |>
+      mutate(name_clean = stringr::str_remove_all(ValleyName, ' '))
+
+    catchments <- cleancatch$name_clean
+  }
+
+  foreach(i = catchments,
+          .combine = dplyr::bind_rows,
+          .multicombine = TRUE) %do% {
+            thiscatch <- readRDS(file.path(datOut, outerdir, which_strict,
+                                           paste0(i, '_', which_strict, '.rds')))
+          }
+}
