7670e9f4635b40f2f22c579fb5292b19d0080b05
= <53928785+galenholt@users.noreply.github.com>
2024-08-06 13:51:39 +1000

    more gandalf changes

--- big_R.sh
+++ big_R.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=90:00:00 # request time (walltime, not compute time)
 #SBATCH --mem=68GB # request memory. This is set up for a main job, so get as much as possible.
 #SBATCH --nodes=1 # number of nodes. Need > 1 to test utilisation
-#SBATCH --ntasks-per-node=10 # Cores per node # I seem to currently be limited to 10 cores.
+#SBATCH --ntasks-per-node=2 # Cores per node # I seem to currently be limited to 10 cores.
 
 #SBATCH -o %x_%A_%a.out # Standard output
 #SBATCH -e %x_%A_%a.err # Standard error
